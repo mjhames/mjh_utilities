a320406c1ab1443b059fe89be727e8f775e72795
mjhames <mrhateman@gmail.com>
2023-08-22 20:41:12 -0700

    Commit refactored code

--- ListFolderSize/ListFolderSize/Program.cs
+++ ListFolderSize/ListFolderSize/Program.cs
@@ -6,19 +6,8 @@ if (args == null || args.Length == 0)
 }
 var showAllFolders = false;
 
-
-if (args.Length > 1)
-{
-    foreach (string arg in args)
-    {
-        switch (arg.ToLower().Replace("-", "").Replace("/", ""))
-        {
-            case "a":
-                showAllFolders = true; break;
-
-        }
-    }
-}
+if (args.Any(a => a.ToLower().Replace("-", "").Replace("/", "") == "a"))
+    showAllFolders = true;
 
 
 ListFolderSize.Functions.ListListDirectoriesWithSizeAndCountInfo(new DirectoryInfo(args[0]), showAllFolders);
